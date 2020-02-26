bd0a99fe8639f8fc256abf67eb57dc9492969dcd
Yunus AYDIN <aydinnyunus@gmail.com>
2020-02-26 22:34:12 +0300

    Update Protect.py

--- Protect.py
+++ Protect.py
@@ -30,12 +30,12 @@ while True:
 
     face_locations = face_recognition.face_locations(rgb_frame)
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+    name = "Unknown"
 
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
 
-        name = "Unknown"
         face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
         best_match_index = np.argmin(face_distances)
         if matches[best_match_index]:
